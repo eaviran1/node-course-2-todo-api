c4972b435250d2a00234f3bc3d8e26d302696746
Eitan-Aviran <eaviran@gmail.com>
2019-03-18 12:45:28 +0200

    Test Post/todos route

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,47 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const Schema = mongoose.Schema;
 
-const User = mongoose.model('User', {
+const userSchema = new Schema({
 	email: {
 		type: String,
 		required: true,
 		trim: true,
-		minlength: 1
-	}
-});
+		minlength: 1,
+		unique: true,
+		validate: {
+			validator: validator.isEmail,
+			message:'${value} is not a valid email'
+		}
+	},
+	password: {
+		type: String,
+		require: true,
+		minlength: 6
+	},
+	tokens: [{
+		access: {
+			type: String,
+			require: true
+		},
+		tokens: {
+			type: String,
+			require: true
+		}
+	}]
+
+}) 
+
+const User = mongoose.model('User', userSchema);
+
+// const User = mongoose.model('User', {
+// 	email: {
+// 		type: String,
+// 		required: true,
+// 		trim: true,
+// 		minlength: 1,
+// 		unique: true
+// 	}
+// });
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
+const _=require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} =require('mongodb');
 
 const{mongoose} = require('./db/mongoose');
+mongoose.set('useCreateIndex', true);
+
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -21,7 +26,21 @@ app.post('/todos', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log('Started on port 3000');
+app.post('/users', (req, res)=> {
+	const body = _.pick(req.body, ['email', 'password']);
+	var  user = new User(body);
+
+	user.save().then((user)=>{
+		res.send(user);
+	}).catch((e)=>{
+		res.status(400).send(e);
+	})
+
 })
+
+app.listen(port, () => {
+	console.log(`Started on port ${port}`);
+});
+
+module.exports = {app};
 
