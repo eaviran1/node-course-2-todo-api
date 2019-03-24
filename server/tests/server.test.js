630fa03233c3fe92a72c6959bbbe11c5764826b1
Eitan-Aviran <eaviran@gmail.com>
2019-03-24 13:30:56 +0200

    Add tests for POST /usrs/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -211,7 +211,7 @@ describe('POST /users', ()=>{
 		.send({	email,password})
 		.expect(200)
 		.expect((res)=>{
-			expect(res.headers['x-auth']).toBeTruthy();
+			expect(res.header['x-auth']).toBeTruthy();
 			expect(res.body._id).toBeTruthy();
 			expect(res.body.email).toBe(email);
 		})
@@ -224,7 +224,7 @@ describe('POST /users', ()=>{
 				expect(user).toBeTruthy();
 				expect(user.password).not.toBe(password);
 				done();
-			})
+			}).catch(e=>done(e))
 		})
 	});
 	it('should return validation errors if request invalid', (done)=>{
@@ -248,4 +248,55 @@ describe('POST /users', ()=>{
 			.end(done)
 	
 	});
+});
+
+describe('POST /users/login',()=>{
+	it('should login user and return auth token ', (done)=>{
+		request(app)
+		.post('/users/login')
+		.send({	
+			email: users[1].email,
+			password:users[1].password
+		})
+		.expect(200)
+		.expect((res)=>{
+			expect(res.header['x-auth']).toBeTruthy()
+		})
+		.end((err, res)=>{
+			if(err)	return done(err);
+
+			User.findById(users[1]._id).then((user)=>{
+				expect(user.toObject().tokens[0]).toMatchObject({
+					access: 'auth', 
+					token: res.header['x-auth']
+				})
+				done();
+			}).catch(e=>done(e))
+		})
+
+	});	
+
+	it('should reject invalid login ', (done)=>{
+		request(app)
+		.post('/users/login')
+		.send({	
+			email: users[1].email+'1',
+			password:users[1].password
+		})
+		.expect(400)
+		.expect((res)=>{
+			expect(res.header['x-auth']).toBeFalsy()
+		})
+		.end((err, res)=>{
+			if(err){
+				return done(err);
+			}
+
+			User.findById(users[1]._id).then((user)=>{
+				expect(user.tokens.length).toBe(0);
+				done();
+			}).catch(e=>done(e))
+		})
+
+	});	
 });
