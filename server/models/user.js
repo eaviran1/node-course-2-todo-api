db6e2c85b08bb6b5de42119eaf32275d087b7293
Eitan-Aviran <eaviran@gmail.com>
2019-03-24 14:23:24 +0200

    Add tests for DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -57,7 +57,7 @@ UserSchema.methods.generateAuthToken = function () {
 UserSchema.methods.removeToken = function(token){
 	const user = this;
 
-	return user.update({
+	return user.updateOne({
 		$pull: {
 			tokens: {token}
 		}
